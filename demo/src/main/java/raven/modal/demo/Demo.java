d769075141c2b37a1c8124ea76849716d6be8ae1
Raven Laing <58245926+DJ-Raven@users.noreply.github.com>
2025-01-26 17:28:49 +0700

    Update to 2.4.0-SNAPSHOT

--- demo/src/main/java/raven/modal/demo/Demo.java
+++ demo/src/main/java/raven/modal/demo/Demo.java
@@ -13,7 +13,7 @@ import java.awt.*;
 
 public class Demo extends JFrame {
 
-    public static final String DEMO_VERSION = "2.3.0";
+    public static final String DEMO_VERSION = "2.4.0-SNAPSHOT";
 
     public Demo() {
         init();
