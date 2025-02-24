1dc3b9672ce97e9f32673d5a5709f93b63e098af
Raven Laing <58245926+DJ-Raven@users.noreply.github.com>
2025-02-24 23:19:56 +0700

    Toast: heavyWeightLayout group toast location

--- modal-dialog/src/main/java/raven/modal/toast/ToastHeavyWeightLayout.java
+++ modal-dialog/src/main/java/raven/modal/toast/ToastHeavyWeightLayout.java
@@ -9,8 +9,10 @@ import raven.modal.toast.option.ToastOption;
 import raven.modal.utils.ModalWindow;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Raven
@@ -23,7 +25,6 @@ public class ToastHeavyWeightLayout extends HeavyWeightRelativeLayout {
 
     @Override
     public void updateLayout() {
-
         if (modalWindows.isEmpty()) return;
 
         boolean reverseOrder = Toast.isReverseOrder();
@@ -31,10 +32,19 @@ public class ToastHeavyWeightLayout extends HeavyWeightRelativeLayout {
         if (reverseOrder) {
             Collections.reverse(Arrays.asList(windowsToastsArray));
         }
+        List<List<ModalWindow>> lists = groupByLocation(windowsToastsArray);
+        for (int i = 0; i < lists.size(); i++) {
+            updateLayout(lists.get(i));
+        }
+    }
+
+    private void updateLayout(List<ModalWindow> list) {
+        if (list.isEmpty()) return;
+
         int lx = 0;
         int ly = 0;
-        for (int i = 0; i < windowsToastsArray.length; i++) {
-            ModalWindow modal = windowsToastsArray[i];
+        for (int i = 0; i < list.size(); i++) {
+            ModalWindow modal = list.get(i);
             ToastPanel toastPanel = (ToastPanel) modal.getContents();
             if (toastPanel.isClose()) continue;
             ToastOption option = toastPanel.getOption();
@@ -55,5 +65,28 @@ public class ToastHeavyWeightLayout extends HeavyWeightRelativeLayout {
                 ly -= y;
             }
         }
+    }
+
+    private List<List<ModalWindow>> groupByLocation(ModalWindow[] modalWindows) {
+        ArrayList<List<ModalWindow>> lists = new ArrayList<>();
+        label:
+        for (int i = 0; i < modalWindows.length; i++) {
+            ModalWindow window = modalWindows[i];
+            ToastPanel toast = (ToastPanel) window.getContents();
+            for (int j = 0; j < lists.size(); j++) {
+                List<ModalWindow> list = lists.get(j);
+                if (list != null && !list.isEmpty()) {
+                    ToastPanel toastPanel = (ToastPanel) list.get(0).getContents();
+                    if (toast.checkSameLayout(toastPanel.getToastData().getOption().getLayoutOption())) {
+                        list.add(window);
+                        continue label;
+                    }
+                }
+            }
+            List<ModalWindow> list = new ArrayList<>();
+            list.add(window);
+            lists.add(list);
+        }
+        return lists;
     }
 }
